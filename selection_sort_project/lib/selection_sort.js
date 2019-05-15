92cfb73fe60508a65aebf50feb1b28165e3d0ee9
your name here <jhspark17@gmail.com>
2019-05-15 15:45:01 -0700

    Do selection sort project

---
+++ selection_sort_project/lib/selection_sort.js
@@ -0,0 +1,32 @@
+function swap(arr, index1, index2) {
+	let temp = arr[index1];
+	arr[index1] = arr[index2];
+	arr[index2] = temp;
+	return arr;
+}
+
+function selectionSort(arr) {//[ 2, -1, 4, 3, 7, 3 ]
+	
+	
+	let minIdx = null;
+	for (let i = 0; i < arr.length; i++) {
+		let num = arr[i];
+		let minimum = Infinity;
+		for (let j = i; j < arr.length; j++) {
+			let tempMin = arr[j];
+
+			if (tempMin < minimum) {
+				minimum = tempMin;
+				minIdx = j;
+			}
+		}
+		arr[i] = minimum;
+		arr[minIdx] = num;
+	}
+	return arr;
+}
+
+module.exports = {
+    selectionSort,
+    swap
+};
