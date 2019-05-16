60e80d44e76014014b32b699e3b1d20a5b29a4fa
your name here <jhspark17@gmail.com>
2019-05-16 13:47:27 -0700

    Did quick sort

--- quick_sort_project/lib/quick_sort.js
+++ quick_sort_project/lib/quick_sort.js
@@ -1,5 +1,13 @@
 function quickSort(array) {
+	if (array.length <= 1) return array;
+	let pivot = array.shift();
+	let left = array.filter(ele => { return ele < pivot});
+	let right = array.filter(ele => {return ele >= pivot});
+	let quickLeft = quickSort(left);
+	let quickRight = quickSort(right);
 
+	let sorted = quickLeft.concat([pivot], quickRight)
+	return sorted
 }
 
 
