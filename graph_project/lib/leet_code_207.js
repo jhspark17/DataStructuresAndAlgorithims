48ab35e0b43ca6cac70c0fb1042281ae225aff15
your name here <jhspark17@gmail.com>
2019-06-20 14:28:34 -0700

    Did courses leet code problem

--- graph_project/lib/leet_code_207.js
+++ graph_project/lib/leet_code_207.js
@@ -1,10 +1,42 @@
 // View the full problem and run the test cases at:
 //  https://leetcode.com/problems/course-schedule/
 
-function canFinish(numCourses, prerequisites) {
-  const graph = createGraph(prerequisites);
-}
+var canFinish = function(numCourses, prerequisites) {
+  let graph = createGraph(prerequisites);
+  let totalCourses = Object.keys(graph).length;
+  let completed = new Set();
+  let eligibleCourseExists = true;
+
+  while (eligibleCourseExists) {
+    eligibleCourseExists = false;
+
+    for (let course in graph) {
+      let everyPreBeenMet = graph[course].every(pre => completed.has(pre));
+
+      if (!completed.has(course) && everyPreBeenMet) {
+        completed.add(course);
+        eligibleCourseExists = true;
+      }
+    }
+  }
+  return completed.size === totalCourses;
+};
 
 function createGraph(prerequisites) {
-  
+  const graph = {};
+
+  prerequisites.forEach(prereq => {
+    [course, pre] = prereq.map(String);
+
+    if (course in graph) {
+      graph[course].push(pre);
+    } else {
+      graph[course] = [pre];
+    }
+
+    if (!(pre in graph)) {
+      graph[pre] = [];
+    }
+  });
+  return graph;
 }
